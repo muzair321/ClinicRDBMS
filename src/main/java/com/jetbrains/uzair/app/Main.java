cb813efbd99fef5761949a9b8a93ed3eb2a8b5e5
muzai <muzairasim321@gmail.com>
2026-01-19 17:49:23 +0500

    made a simple ui to test returnUI()

--- src/main/java/com/jetbrains/uzair/app/Main.java
+++ src/main/java/com/jetbrains/uzair/app/Main.java
@@ -3,20 +3,10 @@ package com.jetbrains.uzair.app;
 import com.jetbrains.uzair.db.PatientDB;
 import com.jetbrains.uzair.model.Patient;
 import com.jetbrains.uzair.model.ValidationException;
+import com.jetbrains.uzair.ui.PatientPanel;
 
 public class Main {
     public static void main(String[] args){
-        try {
-            Patient.addPatient(new Patient("Lisa", 26, "Female"));
-        }catch (ValidationException e){
-            System.err.println("Error: " + e.getMessage());
-        }
-        String[][] print = PatientDB.returnUI();
-        for(String[] sets: print){
-            for(String attrs: sets){
-                System.out.print(attrs + "  ");
-            }
-            System.out.println();
-        }
+        PatientPanel.mainWindow();
     }
 }
--- src/main/java/com/jetbrains/uzair/ui/PatientPanel.java
+++ src/main/java/com/jetbrains/uzair/ui/PatientPanel.java
@@ -1,6 +1,20 @@
 package com.jetbrains.uzair.ui;
 
+import com.jetbrains.uzair.db.PatientDB;
+
 import javax.swing.*;
 
 public class PatientPanel {
+    public static void mainWindow(){
+        JFrame window = new JFrame("Patient Data");
+        window.setSize(200, 400);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        String[] headers = {"ID", "Name", "Age", "Gender"};
+        String[][] data = PatientDB.returnUI();
+        JTable table = new JTable(data, headers);
+        JScrollPane scroll = new JScrollPane(table);
+        window.add(scroll);
+        window.setVisible(true);
+    }
 }
