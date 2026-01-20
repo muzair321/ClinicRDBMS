2afcd8237e7f1210b4b7294d27b3535c77f2c44a
muzai <muzairasim321@gmail.com>
2026-01-20 11:33:55 +0500

    A bit   of testing

--- src/main/java/com/jetbrains/uzair/app/Main.java
+++ src/main/java/com/jetbrains/uzair/app/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args){
         String[] a = {"4", "Zara",  "16", "Female"};
         PatientDB.edit(Patient.check(Patient.convArrayToOb(a)));
+        PatientDB.insert(Patient.check(Patient.convArrayToOb(a)));
         PatientPanel.mainWindow();
     }
 }
