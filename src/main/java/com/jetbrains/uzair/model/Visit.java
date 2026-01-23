9b8d5c6cf93a607c86520ce444c9ecde27c8752f
muzai <muzairasim321@gmail.com>
2026-01-23 23:15:06 +0500

    adding setters and getters to Visit.java

--- src/main/java/com/jetbrains/uzair/model/Visit.java
+++ src/main/java/com/jetbrains/uzair/model/Visit.java
@@ -6,7 +6,7 @@ public class Visit {
     private String treatment;
     private int paid;
     private String date;
-
+    //constructors
     public Visit(){}
     public Visit(int id, int patient_id, String treatment, int paid, String date){
         this.id = id;
@@ -18,5 +18,18 @@ public class Visit {
     public Visit(int patient_id, String treatment, int paid, String date){
         this(-1, patient_id, treatment, paid, date);
     }
-
+    //getters
+    public int getId(){return id;}
+    public int getPatient_id(){return patient_id;}
+    public int getPaid(){return paid;}
+    public String getTreatment(){return treatment;}
+    public String getDate(){return date;}
+    //setters
+    public void setId(int id) {this.id = id;}
+    public void setPatient_id(int patient_id) {this.patient_id = patient_id;}
+    public void setDate(String date) {this.date = date;}
+    public void setPaid(int paid) {this.paid = paid;}
+    public void setTreatment(String treatment) {this.treatment = treatment;}
+    //for ui input
+    p
 }
