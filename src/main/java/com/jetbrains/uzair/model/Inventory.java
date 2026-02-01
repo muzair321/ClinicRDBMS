24b37cfb569227e5c0ebd5e1e0ce49c95959b82d
muzai <muzairasim321@gmail.com>
2026-02-01 20:28:01 +0500

    adding models

---
+++ src/main/java/com/jetbrains/uzair/model/Inventory.java
@@ -0,0 +1,9 @@
+package com.jetbrains.uzair.model;
+
+public class Inventory {
+    private int id;
+    private String name;
+    private String storage;
+    private int amount;
+    private String updatedAt;
+}
