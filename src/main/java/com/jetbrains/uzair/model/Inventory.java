1355ba86e9761fe271163d12a42bdad399441bc2
muzai <muzairasim321@gmail.com>
2026-02-01 20:33:59 +0500

    adding models

--- src/main/java/com/jetbrains/uzair/model/Inventory.java
+++ src/main/java/com/jetbrains/uzair/model/Inventory.java
@@ -1,9 +1,22 @@
 package com.jetbrains.uzair.model;
 
 public class Inventory {
+    //object parameters
     private int id;
     private String name;
     private String storage;
     private int amount;
     private String updatedAt;
+    //constructors
+    public Inventory(){};
+    public Inventory(int id, String name, String storage, int amount, String updatedAt){
+        this.id = id;
+        this.name =  name;
+        this.storage = storage;
+        this.amount = amount;
+        this.updatedAt = updatedAt;
+    }
+    public Inventory(String name, String storage, int amount, String updatedAt){
+        this(-1, name, storage, amount, updatedAt);
+    }
 }
