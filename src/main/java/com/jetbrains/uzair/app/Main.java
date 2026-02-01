48c46dd60941c97094421742b9da5b6638485940
muzai <muzairasim321@gmail.com>
2026-02-01 15:14:57 +0500

    testing

--- src/main/java/com/jetbrains/uzair/app/Main.java
+++ src/main/java/com/jetbrains/uzair/app/Main.java
@@ -1,9 +1,19 @@
 package com.jetbrains.uzair.app;
 
+import com.jetbrains.uzair.db.Database;
 import com.jetbrains.uzair.ui.MainFrame;
+
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args){
+        Database.createTables();
+        try {
+            Database.addTriggers();
+            Database.addIndexes();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         MainFrame.main(args);
     }
 }
