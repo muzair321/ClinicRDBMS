6f6ecd66060f4a0dd03f663a505502129d184a5b
Muhammad Uzair <muzairasim321@gmail.com>
2026-02-03 13:41:02 +0500

    Update InventoryDB.java

--- src/main/java/com/jetbrains/uzair/db/InventoryDB.java
+++ src/main/java/com/jetbrains/uzair/db/InventoryDB.java
@@ -3,7 +3,14 @@ package com.jetbrains.uzair.db;
 import com.jetbrains.uzair.model.Inventory;
 
 public class InventoryDB{
-public static void insert(Inventory i){
-String sql = "INSERT 
+public static void insert(Inventory i) throws SQLException {
+String sql = "INSERT INTO inventory(name, storage, amount) VALUES(?, ?, ?)";
+try(Connection conn = Database.getConnection()){
+PreparedStatement stmt = conn.prepareStatement(sql);
+
+}
+catch(SQLException e){
+throw new SQLException("Error Inserting Data Into Inventory");
+}
 }
 }
