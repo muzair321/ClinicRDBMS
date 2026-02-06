c194a12dd37afa4ddb0f8810a507635021574f3b
fa25-bcs-087 <fa25-bcs-087@isbsrudent.comsats.edu.pk>
2026-02-06 11:39:23 +0500

    added constructors and attributes

--- src/main/java/com/jetbrains/uzair/model/InventoryLogs.java
+++ src/main/java/com/jetbrains/uzair/model/InventoryLogs.java
@@ -1,4 +1,23 @@
 package com.jetbrains.uzair.model;
 
+import com.jetbrains.uzair.db.InventoryLogsDB;
+
 public class InventoryLogs {
+    private int id;
+    private int inventoryId;
+    private int userId;
+    private int amount;
+    private String date;
+
+    public InventoryLogs(){}
+    public InventoryLogs(int id, int inventoryId, int userId, int amount, String date){
+        this.id = id;
+        this.inventoryId = inventoryId;
+        this.userId = userId;
+        this.amount = amount;
+        this.date = date;
+    }
+    public InventoryLogs(int inventoryId, int userId, int amount, String date){
+        this(-1, inventoryId, userId, amount, date);
+    }
 }
