1c9a3b9770978ab8de45dc773c4f97c7dab0e60d
muzai <muzairasim321@gmail.com>
2026-02-07 22:29:56 +0500

    added file checks

---
+++ src/main/java/com/jetbrains/uzair/app/FileChecker.java
@@ -0,0 +1,12 @@
+package com.jetbrains.uzair.app;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileChecker {
+    public static boolean exists() {
+        String userHome = System.getProperty("user.home");
+        Path path = Path.of("jdbc:sqlite:" + userHome + "/clinicData.db");
+        return Files.exists(path);
+    }
+}
--- src/main/java/com/jetbrains/uzair/app/Main.java
+++ src/main/java/com/jetbrains/uzair/app/Main.java
@@ -2,13 +2,21 @@ package com.jetbrains.uzair.app;
 
 import com.jetbrains.uzair.db.Database;
 import com.jetbrains.uzair.ui.LoginFrame;
-import com.jetbrains.uzair.ui.MainFrame;
 
 import javax.swing.*;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args){
+        if(!FileChecker.exists()) {
+            Database.createTables();
+            try {
+                Database.addTriggers();
+                Database.addIndexes();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
         SwingUtilities.invokeLater(LoginFrame::new);
     }
 }
