42180432b680130f4ab4197ddd75f6d6d070f995
muzai <muzairasim321@gmail.com>
2026-02-08 18:44:22 +0500

    brute testing tables

--- src/main/java/com/jetbrains/uzair/app/Main.java
+++ src/main/java/com/jetbrains/uzair/app/Main.java
@@ -1,6 +1,9 @@
 package com.jetbrains.uzair.app;
 
 import com.jetbrains.uzair.db.Database;
+import com.jetbrains.uzair.db.PatientDB;
+import com.jetbrains.uzair.model.Inventory;
+import com.jetbrains.uzair.model.Patient;
 import com.jetbrains.uzair.ui.FirstUser;
 import com.jetbrains.uzair.ui.LoginFrame;
 
@@ -21,5 +24,11 @@ public class Main {
         }else {
             new LoginFrame();
         }
+//        for(int i = 0; i < 12000; i++){
+//            Patient p = new Patient("Uzaor" + i, 20, "Male", null , null );
+//                try{PatientDB.insert(p);} catch (SQLException e) {
+//                    JOptionPane.showMessageDialog(null, "Fked");
+//                }
+//        }
     }
 }
