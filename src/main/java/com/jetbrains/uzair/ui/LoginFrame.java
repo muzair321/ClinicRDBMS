c9b18311e0fbc465397b29c69f45c310daad26d4
muzai <muzairasim321@gmail.com>
2026-02-08 12:45:53 +0500

    minor polishing

--- src/main/java/com/jetbrains/uzair/ui/LoginFrame.java
+++ src/main/java/com/jetbrains/uzair/ui/LoginFrame.java
@@ -7,6 +7,7 @@ import com.jetbrains.uzair.model.User;
 import javax.swing.*;
 import java.awt.*;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class LoginFrame extends JFrame {
 
@@ -15,17 +16,42 @@ public class LoginFrame extends JFrame {
 
     public LoginFrame() {
         setTitle("Clinic Login");
-        setSize(350, 220);
+        setSize(350, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
+        setResizable(false);
+        add(header(), BorderLayout.NORTH);
         add(buildForm(), BorderLayout.CENTER);
         add(buildButtons(), BorderLayout.SOUTH);
 
         setVisible(true);
     }
 
+    private JPanel header(){
+        JPanel header = new JPanel(new BorderLayout());
+        header.setBorder(BorderFactory.createEmptyBorder(15, 20, 15, 20));
+        header.setBackground(new Color(12, 38, 78));
+
+        ImageIcon logo = new ImageIcon(Objects.requireNonNull(MainFrame.class.getResource("/img/Clinic-Uncolored.png")));
+        Image scaled = logo.getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH);
+        JLabel logoLabel = new JLabel(new ImageIcon(scaled));
+
+        JLabel title = new JLabel("User Log In");
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("Segoe UI", Font.BOLD, 18));
+
+        JPanel leftHeader = new JPanel();
+        leftHeader.setLayout(new BoxLayout(leftHeader, BoxLayout.X_AXIS));
+        leftHeader.setOpaque(false); // keep header background visible
+        leftHeader.add(logoLabel);
+        leftHeader.add(Box.createHorizontalStrut(70));
+
+        header.add(leftHeader, BorderLayout.WEST);
+        header.add(title, BorderLayout.CENTER);
+        return header;
+    }
     private JPanel buildForm() {
         JPanel panel = new JPanel(new GridBagLayout());
         panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 10, 20));
@@ -57,7 +83,7 @@ public class LoginFrame extends JFrame {
     }
 
     private JPanel buildButtons() {
-        JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JButton loginBtn = new JButton("Login");
 
         loginBtn.addActionListener(_ -> login());
@@ -65,6 +91,7 @@ public class LoginFrame extends JFrame {
         // Press ENTER to log in
         getRootPane().setDefaultButton(loginBtn);
 
+        panel.add(Box.createHorizontalStrut(20));
         panel.add(loginBtn);
         return panel;
     }
