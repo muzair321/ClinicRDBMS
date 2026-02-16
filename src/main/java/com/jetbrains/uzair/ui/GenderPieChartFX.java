1ff6687eb56c3cb323225d73f33e2acd58dbc576
muzai <muzairasim321@gmail.com>
2026-02-16 14:42:44 +0500

    pie chart for visitor gender

---
+++ src/main/java/com/jetbrains/uzair/ui/GenderPieChartFX.java
@@ -0,0 +1,60 @@
+package com.jetbrains.uzair.ui;
+
+import com.jetbrains.uzair.db.AnalyticsDB;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Tooltip;
+
+import java.util.LinkedHashMap;
+
+public class GenderPieChartFX {
+    private static ObservableList<PieChart.Data> pieData;
+    private static PieChart chart;
+    public static JFXPanel start(){
+        JFXPanel panel = new JFXPanel();
+        LinkedHashMap<String, Integer> chartMap = AnalyticsDB.genderToday();
+        pieData = FXCollections.observableArrayList();
+        chartMap.forEach((name, value) -> {
+            pieData.add(new PieChart.Data(name, value));
+        });
+        chart = new PieChart(pieData);
+        chart.setLabelsVisible(false);
+        chart.setAnimated(false);
+        chart.setTitle("Gender Distribution");
+        for (PieChart.Data data1 : chart.getData()) {
+            Tooltip tooltip = new Tooltip(data1.getName() + ": " + (int)(data1.getPieValue()));
+            tooltip.setShowDelay(javafx.util.Duration.seconds(0.1));
+            // The getNode() method returns the actual graphical bar/node
+            Tooltip.install(data1.getNode(), tooltip);
+        }
+        Scene scene = new Scene(chart, 960, 300);
+        String css = BarChartFX.class.getResource("/dashboard.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        panel.setScene(scene);
+        return panel;
+    }
+    public static void update() {
+        // Get fresh data from DB on the calling thread
+        LinkedHashMap<String, Integer> newData = AnalyticsDB.genderToday();
+
+        Platform.runLater(() -> {
+            // 1. Clear existing data points
+            pieData.clear();
+            // 2. Add new data points (Chart updates automatically)
+            newData.forEach((name, value) -> {
+                pieData.add(new PieChart.Data(name, value));
+            });
+            for (PieChart.Data data1 : chart.getData()) {
+                Tooltip tooltip = new Tooltip(data1.getName() + data1.getPieValue());
+                tooltip.setShowDelay(javafx.util.Duration.seconds(0.1));
+                // The getNode() method returns the actual graphical bar/node
+                Tooltip.install(data1.getNode(), tooltip);
+            }
+        });
+    }
+}
