14974aca4dc6cb035299375678def60587d6dcf0
muzai <muzairasim321@gmail.com>
2026-02-16 14:44:03 +0500

    jpanel for all pie charts

---
+++ src/main/java/com/jetbrains/uzair/ui/PieCharts.java
@@ -0,0 +1,13 @@
+package com.jetbrains.uzair.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PieCharts {
+    public static JPanel mainFrame(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.add(GenderPieChartFX.start());
+        return panel;
+    }
+}
