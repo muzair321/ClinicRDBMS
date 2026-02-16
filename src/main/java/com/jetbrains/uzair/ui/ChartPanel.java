0f9425eb84339a3dde918a64ec330811f115592f
muzai <muzairasim321@gmail.com>
2026-02-16 13:48:15 +0500

    all JFXpanels into a Jpanel

--- src/main/java/com/jetbrains/uzair/ui/ChartPanel.java
+++ src/main/java/com/jetbrains/uzair/ui/ChartPanel.java
@@ -1,23 +1,15 @@
 package com.jetbrains.uzair.ui;
 
-import com.jetbrains.uzair.db.FolderCreation;
-
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 
 public class ChartPanel {
     public static JPanel mainFrame(){
         JPanel frame = new JPanel(new BorderLayout());
-        String userHome = null;
-        try {
-            userHome = FolderCreation.createDirectories();
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
-        String URL = userHome + "/clinicData.db";
-        BarChartFX chart = new BarChartFX(URL);
-        frame.add(chart, BorderLayout.CENTER);
+        frame.setBounds(new Rectangle(1920, 785));
+
+        frame.add(BarChartFX.start(), BorderLayout.NORTH );
+        frame.add(PieCharts.mainFrame(), BorderLayout.CENTER);
         return frame;
     }
 }
